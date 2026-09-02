6a6bb598d2a2453a1b164733051a4b1278021006
sabrina <sabrinahassan3@hotmail.com>
2026-09-02 20:22:51 +0100

    The skeleton of it
    Added includes for mock hardware and updated evaluation logic.

--- src/logic.c
+++ src/logic.c
@@ -1,12 +1,18 @@
 // This is the only file you should edit.
 
 #include "logic.h"
+#include "mock_hw.h"
 
 SystemOutputs evaluate_system_state(BmsData bms, BatteryTempData batt_temp,
                                      InverterData inverter, PvData pv,
                                      GridData grid, RelayState relay,
                                      DoorSwitchData door) {
-    SystemOutputs out = {0};
+  (void)bms; (void)batt_temp; (void)inverter;
+  (void)pv; (void)grid; (void)relay; (void)door;
+
+  
+  SystemOutputs out = {0};
+  out.state = SYSTEM_OFF; // this is gna be the fsil safe thing till the logic gets aded
     // TODO: implement
     return out;
 }
