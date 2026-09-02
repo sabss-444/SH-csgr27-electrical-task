3df11985f2dda42f27a2bcc6f26c1a0fa9a2dba1
sabrina <sabrinahassan3@hotmail.com>
2026-09-02 20:37:56 +0100

    Battery cell min and max
    Added comments regarding battery specifications and thresholds.
    Using the fsuk pdf to find the thresholds

--- src/logic.c
+++ src/logic.c
@@ -1,5 +1,12 @@
 // This is the only file you should edit.
-
+// im assuming the home battery is using the same as the fsuk data sheet cos idk wherre to get the thresholds
+// 32 cells in series, nominal pack voltage 102.4v
+// max pack 116.8v per cell
+// so divide by 32 
+// nominal 102.4 / 32 = 3.2v (3200mV)
+//max 116.8 / 32 = 3.65v (3650mV)
+//google says minimum safe voltage is 2.5v 
+// range 2500 - 3650 
 #include "logic.h"
 #include "mock_hw.h"
 
